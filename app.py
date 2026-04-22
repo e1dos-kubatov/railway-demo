d53e5275b0364e59e14ef7c80571a7b600ee145c
Eldos Kubatov <eldos.220505@email.com>
2026-04-22 22:38:17 +0600

    Fixed errors

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Привет из Railway!</h1>'
+    
+    return '<h1>РџСЂРёРІРµС‚ РѕС‚ Railway!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
